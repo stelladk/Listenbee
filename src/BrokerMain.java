6add897a78011db51df34afd1050bd86c0350e17
Lambros Lountzis <lambroslntz15@gmail.com>
2020-03-18 16:11:26 +0200

    malakies

---
+++ src/BrokerMain.java
@@ -0,0 +1,5 @@
+public class BrokerMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ConsumerMain.java
@@ -0,0 +1,5 @@
+public class ConsumerMain {
+    public static void main(String[] args) {
+
+    }
+}
