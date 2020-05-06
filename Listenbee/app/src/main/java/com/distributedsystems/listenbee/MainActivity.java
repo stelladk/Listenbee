7b8d0af61361ca4c237becc2e214be9e8916191b
Lambros Lountzis <lambroslntz15@gmail.com>
2020-05-06 19:26:01 +0300

    Bottom navigation bar

--- Listenbee/app/src/main/java/com/distributedsystems/listenbee/MainActivity.java
+++ Listenbee/app/src/main/java/com/distributedsystems/listenbee/MainActivity.java
@@ -1,55 +1,60 @@
 package com.distributedsystems.listenbee;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //Bottom navigation toolbar
+        tabs = findViewById(R.id.bottom_navigation);
+        tabs.setOnNavigationItemSelectedListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//    }
+
+    /**
+     * Handles bottom navigation bar item clicks.
+     * Loads the right fragment for each item
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+
+        switch(item.getItemId()){
+            case R.id.action_library:
+                //TODO
+               break;
+            case R.id.action_profile:
+                //TODO
+                break;
+            case R.id.action_search:
+                //TODO
+                break;
+            default:
+                //TODO
+                return false;
         }
 
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
