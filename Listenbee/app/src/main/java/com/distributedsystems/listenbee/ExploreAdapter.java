a55a1aba514ffff8aa6556550971aff9155dd4d3
Stella Douka <35050451+stelladk@users.noreply.github.com>
2020-05-18 11:43:00 +0300

    ExploreAdapter

---
+++ Listenbee/app/src/main/java/com/distributedsystems/listenbee/ExploreAdapter.java
@@ -0,0 +1,82 @@
+package com.distributedsystems.listenbee;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.distributedsystems.listenbee.fragments.ForYouFragment;
+import com.example.eventdeliverysystem.musicFile.MusicFile;
+
+import android.net.Uri;
+import java.util.List;
+
+public class ExploreAdapter extends RecyclerView.Adapter<ExploreAdapter.ExploreViewHolder> {
+    private Context context;
+    private List<MusicFile> items;
+
+    public ExploreAdapter(Context context, @NonNull List<MusicFile> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ExploreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.song, parent, false);
+        return new ExploreViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExploreViewHolder holder, final int position) {
+        final MusicFile track_preview = items.get(position);
+
+        byte[] cover = track_preview.getCover();
+        final String title = track_preview.getTrackName();
+        final String artist = track_preview.getArtistName();
+
+        if(cover != null){
+            holder.image.setImageBitmap(BitmapFactory.decodeByteArray(cover, 0, cover.length));
+        }
+        holder.trackName.setText(title);
+        holder.artistName.setText(artist);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ForYouFragment.getData(title, artist);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    static class ExploreViewHolder extends RecyclerView.ViewHolder{
+
+        View itemView;
+        ImageView image;
+        TextView trackName;
+        TextView artistName;
+        ImageButton download_btn;
+
+        ExploreViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+            image = itemView.findViewById(R.id.song_cover);
+            trackName = itemView.findViewById(R.id.song_title);
+            artistName = itemView.findViewById(R.id.song_artist);
+            download_btn = itemView.findViewById(R.id.download_btn);
+        }
+    }
+}
--- Listenbee/app/src/main/java/com/distributedsystems/listenbee/fragments/ForYouFragment.java
+++ Listenbee/app/src/main/java/com/distributedsystems/listenbee/fragments/ForYouFragment.java
@@ -1,6 +1,7 @@
 package com.distributedsystems.listenbee.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.distributedsystems.listenbee.ExploreAdapter;
+import com.distributedsystems.listenbee.LibraryAdapter;
 import com.distributedsystems.listenbee.MainActivity;
 import com.distributedsystems.listenbee.R;
 import com.example.eventdeliverysystem.Consumer;
@@ -17,7 +20,7 @@ import java.util.List;
 
 public class ForYouFragment extends Fragment {
 
-    private Consumer consumer;
+    private static Consumer consumer;
     private List<MusicFile> available_songs = null;
 
     @Nullable
@@ -31,5 +34,16 @@ public class ForYouFragment extends Fragment {
     public void loadLibrary(){
         consumer = MainActivity.getConsumer();
         available_songs = consumer.loadLibrary();
+
+        if(available_songs != null){
+            ExploreAdapter adapter = new ExploreAdapter(getContext(), available_songs);
+        }else{
+            Log.d("LOAD LIBRARY", "Could not get available songs");
+        }
+    }
+
+    public static void getData(String trackName, String artistName){
+//        MusicFile track = consumer.playData(trackName, artistName, "ONLINE");
+//        MainActivity.playOnClick(track);
     }
 }
